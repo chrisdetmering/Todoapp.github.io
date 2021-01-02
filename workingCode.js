9a6e3790ec48eea68a3f7c1a95335499f81fab60
Josh Hattery <74947992+sdjosh87@users.noreply.github.com>
2021-01-02 13:36:16 -0800

    Change CSS and add some JS

--- workingCode.js
+++ workingCode.js
@@ -1,1 +1,21 @@
-const button = onclick()
+const dayInput = document.getElementById("dayInput");
+const taskInput = document.getElementById("taskInput");
+const btnAddtask = document.getElementById("btnAddtask");
+const lsOutput = document.getElementById("lsOutput");
+
+btnAddtask.onclick = function () {
+    const day = dayInput.Value;
+    const task = taskInput.Value;
+
+    if (day && task) {
+        localStorage.setItem(day, task);
+        location.reload();
+    }
+};
+
+for (let i = 0; i < localStorage.length; i++) {
+    const day = localStorage.day(i);
+    const task = localStorage.getItem(day);
+
+    lsOutput.innerHTML += '{day}: {task}';
+}
