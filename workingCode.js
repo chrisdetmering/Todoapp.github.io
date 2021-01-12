cd4563a5ba146361601987166ed7fccd603a4f00
Josh Hattery <74947992+sdjosh87@users.noreply.github.com>
2021-01-11 16:07:13 -0800

    Adjusting code. Still needs work

--- workingCode.js
+++ workingCode.js
@@ -1,20 +1,2 @@
-const box1 = document.getElementsById('box1');
-const box2 = document.getElementsById('box2');
-const lsOutput = document.getElementById('lsOutput');
-const addtaskbtn = document.getElementsById('addtaskbtn').addEventListener('click', buttonClick);
+const inpBox = document.getElementsById('taskBox');
 
-function buttonClick(){
-
-    if (box1 && box2) {
-        localStorage.setItem(box1, box2);
-        location.reload();
-    }
-};
-
-for (let i = 0; i < localStorage.length; i++) {
-    const box1 = localStorage.box1(i);
-    const box2 = localStorage.getItem(box2);
-
-    lsOutput.innerHTML += (box1, box2);
-
-}
