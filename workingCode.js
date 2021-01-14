89a7836f59dada459378c58a6840c8a6299d2605
froggy <chrisdetmering@gmail.com>
2021-01-14 13:44:32 -0800

    Code review refactoring completed

--- workingCode.js
+++ workingCode.js
@@ -1,47 +1,64 @@
-// Selectors
 document.querySelector('form').addEventListener('submit', handleSubmitForm);
 document.querySelector('ul').addEventListener('click', handleClickDeleteOrCheck);
 document.getElementById('clearList').addEventListener('click', handleClearList);
 
-// Event Handlers
 function handleSubmitForm(e) {
     e.preventDefault();
-    let input = document.querySelector('input');
-    if (input.value != '')
+    const input = document.querySelector('input');
+    if (input.value !== '') { 
         addTodo(input.value);
-    input.value = ''
+        input.value = ''; 
+    }
 }
-function handleClickDeleteOrCheck(e) {
-    if (e.target.name == 'addTaskBtn')
-        checkTodo(e);
 
-    if (e.target.name == 'deleteBtn')
+function handleClickDeleteOrCheck(e) {
+    if (e.target.name == 'addTaskBtn') { 
+        checkTodo(e);
+        return; 
+    }
+       
+
+    if (e.target.name == 'deleteBtn') { 
         deleteTodo(e);
+        return; 
+    }
+       
 }
+
 function handleClearList(e) {
     document.querySelector('ul').innerHTML = '';
 }
 
-// Helpers
 function addTodo(todo) {
-    let ul = document.querySelector('ul');
-    let li = document.createElement('li');
+    const ul = document.querySelector('ul');
+    const li = document.createElement('li');
+    const span = document.createElement('span'); 
+    const addButton = document.createElement('button'); 
+    const deleteBtn = document.createElement('button'); 
 
-    li.innerHTML = `
-        <span class="todo-item">${todo}</span>
-        <button name="addTaskBtn"><i class="fas fa-check-square"></i></button>
-        <button name="deleteBtn"><i class="fas fa-trash-alt"></i></button>
-    `;
+    //to prevent cross site scripting (XSS) . 
+    span.classList.add("todo-item"); 
+    span.textContent = todo; 
+    addButton.setAttribute("name", "addTaskBtn"); 
+    deleteBtn.setAttribute("name", "deleteBtn"); 
+    
+    //okay because we are not using user input
+    addButton.innerHTML = `<i class="fas fa-check-square"></i>`; 
+    deleteBtn.innerHTML = `<i class="fas fa-trash-alt"></i>`
+
+    li.appendChild(span); 
+    li.appendChild(addButton); 
+    li.appendChild(deleteBtn); 
     ul.appendChild(li);
 }
 function checkTodo(e) {
-    let item = e.target.parentNode;
+    const item = e.target.parentNode;
     if (item.style.textDecoration == 'line-through')
         item.style.textDecoration = 'none';
     else 
         item.style.textDecoration = 'line-through';
 }
 function deleteTodo(e) {
-    let item = e.target.parentNode;
+    const item = e.target.parentNode;
     item.remove();
 }
